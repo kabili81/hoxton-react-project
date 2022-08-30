1cc42e326f61dec4093eafd1e02b880573009bf3
Kabil Ibrahimi <kabili81@hotmail.com>
2022-08-30 14:39:19 +0200

    update

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -7,16 +7,18 @@ type Quote = {
   incomingInvoces: number;
   outgoingInvoces: number;
 };
-const quote = {
-  name: "Kabil",
-  email: "kabil@email.com",
-  phone: 38344847761,
-  employee: 5,
-  incomingInvoces: 250,
-  outgoingInvoces: 500,
-};
+// const quote = {
+//   name: "Kabil",
+//   email: "kabil@email.com",
+//   phone: 38344847761,
+//   employee: 5,
+//   incomingInvoces: 250,
+//   outgoingInvoces: 500,
+// };
 export function Contact() {
   const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [name,setName] = useState('')
+  const [emails,setEmails] = useState('')
   return (
     <main className="main">
       <h1>Contact</h1>
@@ -25,7 +27,7 @@ export function Contact() {
         <h2>Request A Quote</h2>
       </div>
       <div className="form">
-        <form
+        {/* <form
           className="quote-form"
           onSubmit={(event) => {
             event.preventDefault();
@@ -45,6 +47,56 @@ export function Contact() {
           <input type="text" name="name" placeholder="Name*" />
 
           <input type="text" name="email" placeholder="Email*" />
+
+          <input type="text" name="phone" placeholder="Phone Numer*" />
+
+          <input
+            type="number"
+            min={0}
+            name="employee"
+            placeholder="Employee Number*"
+          />
+
+          <input
+            type="number"
+            min={0}
+            name="incoming"
+            placeholder="Number of incoming invoces*"
+          />
+
+          <input
+            type="number"
+            min={0}
+            name="outgoing"
+            placeholder="Number of outgoing invoces*"
+          />
+
+          <button className="button">Send</button>
+        </form> */}
+        <form
+          className="quote-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            const quote = {
+              name: event.target.name.value,
+              email: event.target.email.value,
+              phone: event.target.phone.value,
+              employee: event.target.employee.valueAsNumber,
+              incomingInvoces: event.target.incoming.valueAsNumber,
+              outgoingInvoces: event.target.outgoing.valueAsNumber,
+            };
+
+            setQuotes([...quotes, quote]);
+          }}
+        >
+          <input type="text" name="name" placeholder="Name*" onChange={event => {
+            setName(event.target.value)
+          }} value={name}/>
+
+          <input type="text" name="email" placeholder="Email*"onChange={event => {
+            setEmails(event.target.value)
+          }} value={emails} />
 
           <input type="text" name="phone" placeholder="Phone Numer*" />
 
